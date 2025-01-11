cb292bec206ead915e8063707950552eae3eda25
Patrick Mast <patrickmast@mac.com>
2025-01-11 15:14:18 +0100

    fix: update vite and typescript configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
-export default defineConfig({
+const config = {
   plugins: [react()],
   build: {
     outDir: 'dist'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
-});
+}
+
+export default defineConfig(config)
