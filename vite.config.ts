d57895f65d51efe6f9c7b3edfc2866e1069f26fe
Patrick Mast <patrickmast@mac.com>
2025-01-11 15:11:41 +0100

    fix: use ESM syntax in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-const { defineConfig } = require('vite');
-const react = require('@vitejs/plugin-react-swc');
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-module.exports = defineConfig({
+export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist'
