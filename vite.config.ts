b6fa7b685b1a093f5b158f006bb263e964bed0ce
Patrick Mast <patrickmast@mac.com>
2025-01-11 15:09:21 +0100

    fix: switch to commonjs syntax for vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+const { defineConfig } = require('vite');
+const react = require('@vitejs/plugin-react-swc');
 
-export default defineConfig({
+module.exports = defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist'
